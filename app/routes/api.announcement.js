b2f3f3633ab24c911b3ce97abfe11c03a2882c60
HarshAgrawalSB <harshagrawal@startbitsolutions.com>
2024-11-29 16:20:44 +0530

    updated api for cors error

--- app/routes/api.announcement.js
+++ app/routes/api.announcement.js
@@ -1,28 +1,19 @@
 import { json } from "@remix-run/node";
-
 import { getAnnouncementDetails } from "../announcement.server";
 import { cors } from "remix-utils/cors";
 
 export async function loader({ request }) {
-    // this now uses the Authorization header to authenticate the user
-    //   let user = await authenticateWithHeader(request)
+    // Extract the shopUrl from the query parameters
     const url = new URL(request.url);
     const shopUrl = url.searchParams.get('shopUrl');
 
-    // if (request.method === "OPTIONS") {
-    //     const response = json({
-    //         status: 200,
-    //     });
-    //     return await cors(request, response);
-    // }
-
+    // Fetch the announcement data based on the shopUrl
     let announcementData = await getAnnouncementDetails(shopUrl);
 
-    const response = json({ announcementData })
+    // Create the response object
+    const response = json({ announcementData });
+
     await cors(request, response);
 
-
-    return json({ announcementData });
-
-    // return cors(request, response)
+    return response;
 }
